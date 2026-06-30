7a71496f33bac9b259cbdf03f652a993bc279c12
Tomás Andrés Cáceres-Mansilla <tomasandrescaceresm@gmail.com>
2026-06-29 21:11:07 -0400

    Add copyright and license information to main .py file
    Added copyright information and licensing details to the file.

--- src/roche-lobe-dynamics.py
+++ src/roche-lobe-dynamics.py
@@ -1,3 +1,19 @@
+# roche-lobe-dynamics
+# Copyright (C) 2026 Tomás Andrés Cáceres-Mansilla
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 The simulation initializes with the 2 mass sources opposite to each other along the x axis.
 
